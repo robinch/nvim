b6b8ba22218884b63f8e7e64385d00bacdb6de59
Robin Chowdhury <robinch88@gmail.com>
2024-04-16 16:40:11 +0200

    add next and prev shortcuts for quickfix list

--- after/plugin/remap.lua
+++ after/plugin/remap.lua
@@ -25,6 +25,8 @@ vim.keymap.set("n", "<leader>fd", vim.lsp.buf.format, {desc = '[F]ormat [D]ocume
 
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>qn", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>qp", "<cmd>cprev<CR>zz")
 -- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 -- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
