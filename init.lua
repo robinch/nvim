de3c74be9f9fd2b6ccde7f9e1a20b0e9d07ed942
Robin Chowdhury <robinch88@gmail.com>
2025-02-28 19:12:36 +0100

    formatting

--- init.lua
+++ init.lua
@@ -108,6 +108,7 @@ require('lazy').setup({
       lspconfig.html.setup({
         capabilities = capabilities,
       })
+
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
@@ -199,7 +200,19 @@ require('lazy').setup({
   { 'numToStr/Comment.nvim', opts = {} },
 
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+    }
+  },
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built.
   -- Only load if `make` is available. Make sure you have the system
